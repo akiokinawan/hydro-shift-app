1bde5ae29728bc0633958af65cd70f54a6926dec
akiokinawan <yomitan15@gmail.com>
2025-07-21 16:56:52 +0900

    PWAの追加

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -20,6 +20,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Hydro-Shift</title>
         <link rel="icon" href="/icon.png" />
         <meta name="description" content="畑の水かけ当番管理システム" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1976d2" />
+        <link rel="icon" href="/icon-192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/icon-192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/icon-512.png" sizes="512x512" />
       </Head>
       <Layout>
         <Component {...pageProps} />
